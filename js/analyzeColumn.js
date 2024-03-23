87dcfc99e749fa666b22c9f89cc98f92662b5f17
zeyad mohammed nagi <zeyadnagi421@gmail.com>
2024-03-24 01:42:12 +0200

    solve some issue

--- js/analyzeColumn.js
+++ js/analyzeColumn.js
@@ -13,20 +13,20 @@ function analyzeColumn() {
 }
 
 function dataProcessing(colName) {
-  //.log(colName);
-
   var index = csvData.columns.findIndex((column) => column[0] === colName);
   var stValues = csvData.columns[index].slice(1);
 
   var Values = stValues.map(Number);
-
-  //.log(Values);
 
   const mean = () => {
     let total = 0;
     for (let i = 0; i < Values.length; i++) {
       total += Values[i];
     }
+    document.getElementById("mean").innerHTML = `Mean of ${colName}: ${
+      total / Values.length
+    }`;
+
     return total / Values.length;
   };
 
@@ -34,8 +34,14 @@ function dataProcessing(colName) {
     Values.sort((a, b) => a - b);
     let n = Values.length;
     if (n % 2 == 0) {
+      document.getElementById("median").innerHTML = `Median of   ${colName}: ${
+        (Values[n / 2 - 1] + Values[n / 2]) / 2
+      }`;
       return (Values[n / 2 - 1] + Values[n / 2]) / 2;
     } else {
+      document.getElementById("median").innerHTML = `Median of ${colName}: ${
+        Values[(n - 1) / 2]
+      }`;
       return Values[Math.floor((n - 1) / 2)];
     }
   };
@@ -56,20 +62,31 @@ function dataProcessing(colName) {
         modes.push(x);
       }
     }
-
+    document.getElementById(
+      "mode"
+    ).innerHTML = `Mode/s of ${colName}: ${modes}`;
     return modes;
   };
 
   const max = () => {
+    document.getElementById(
+      "max"
+    ).innerHTML = `Max value in ${colName}: ${Math.max(...Values)}`;
     return Math.max(...Values);
   };
 
   const min = () => {
+    document.getElementById(
+      "min"
+    ).innerHTML = `Min value in ${colName}: ${Math.min(...Values)}`;
     return Math.min(...Values);
   };
 
   const range = () => {
-    return max() - min();
+    document.getElementById(
+      "range"
+    ).innerHTML = `Range of values in ${colName}: ${`${min()} - ${max()}`}`;
+    return `${min()} - ${max()}`;
   };
 
   const standardDeviation = () => {
@@ -79,34 +96,38 @@ function dataProcessing(colName) {
     );
     const variance =
       squaredDifferences.reduce((acc, curr) => acc + curr, 0) / Values.length;
+    document.getElementById(
+      "standardDeviation"
+    ).innerHTML = `Standard Deviation of ${colName}: ${Math.sqrt(variance)}`;
     return Math.sqrt(variance);
   };
 
-  //   //.log("Mean:", mean());
-  //   //.log("Median:", median());
-  //   //.log("Mode:", mode());
-  //   //.log("Max:", max());
-  //   //.log("Min:", min());
-  //   //.log("Range:", range());
-  //   //.log("Standard Deviation:", standardDeviation());
+  // const text = `Mean of ${colName} is ${mean()}. \n Median of ${colName} is ${median()}. \n
+  // Mode of ${colName} is ${mode()}. \n
+  // Max of ${colName} is ${max()}. \n
+  // Min of ${colName} is ${min()}. \n
+  // Range of ${colName} is ${range()}. \n
+  // Standard Deviation of ${colName} is ${standardDeviation()}. \n
+  // `;
 
-  const text = `Mean of ${colName} is ${mean()}. \n Median of ${colName} is ${median()}. \n
-  Mode of ${colName} is ${mode()}. \n
-  Max of ${colName} is ${max()}. \n
-  Min of ${colName} is ${min()}. \n
-  Range of ${colName} is ${range()}. \n
-  Standard Deviation of ${colName} is ${standardDeviation()}. \n
-  `;
+  // if (colName == old) {
+  //   alert("Please change the column name");
+  // } else {
+  //   dataShow.innerHTML = "";
+  //   for (let i = 0; i < text.length; i++) {
+  //     setTimeout(function () {
+  //       dataShow.innerHTML += text.charAt(i);
+  //     }, 15 * i);
+  //   }
+  // }
 
-  if (colName == old) {
-    alert("Please change the column name");
-  } else {
-    dataShow.innerHTML = "";
-    for (let i = 0; i < text.length; i++) {
-      setTimeout(function () {
-        dataShow.innerHTML += text.charAt(i);
-      }, 15 * i);
-    }
-  }
+  mean();
+  median();
+  mode();
+  max();
+  min();
+  range();
+  standardDeviation();
+
   old = colName;
 }
